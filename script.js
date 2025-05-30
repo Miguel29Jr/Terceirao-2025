1b2aa8607e0ac58d2e9246ebe4cfbb6a8c341110
Miguel29Jr <163026282+Miguel29Jr@users.noreply.github.com>
2025-05-30 14:21:14 -0300

    Add files via upload
    códigos do dia 30/05

--- script.js
+++ script.js
@@ -17,4 +17,8 @@ botaoDeAcessibilidade.addEventListener('click', function (){
     botaoDeAcessibilidade.classList.toggle('rotacao-botao');
     opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
    })
+   const alternaContraste = document.getElementById('alterna-contraste')
+   alternaContraste.addEventListener('click', function(){
+    document.body.classList.toggle('alto-contraste')
+})
 });
