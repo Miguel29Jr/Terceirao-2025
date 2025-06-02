61ad26361afa52bb733e195e38ceac961beee7ab
Miguel29Jr <163026282+Miguel29Jr@users.noreply.github.com>
2025-06-02 13:50:52 -0300

    Add files via upload
    Projeto finalizado!
    Dia: 02\06

--- script.js
+++ script.js
@@ -20,5 +20,11 @@ botaoDeAcessibilidade.addEventListener('click', function (){
    const alternaContraste = document.getElementById('alterna-contraste')
    alternaContraste.addEventListener('click', function(){
     document.body.classList.toggle('alto-contraste')
+const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') ==='true';
+botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado)
 })
+ScrollReveal().reveal('#inicio', { delay: 500 });
+ScrollReveal().reveal('#tropicalia', { delay: 500 });
+ScrollReveal().reveal('#galeria', { delay: 500 });
+ScrollReveal().reveal('#contato', { delay: 500 });
 });
